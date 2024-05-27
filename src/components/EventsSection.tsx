1f48a3bb1913453d2d1c4b2378d4ba0cb1ddacb8
banushi-a <altonbanushi@icloud.com>
2024-05-26 19:57:31 -0500

    Abstract Event Section

--- src/components/EventsSection.tsx
+++ src/components/EventsSection.tsx
@@ -1,9 +1,18 @@
 import TwoBros from "../public/2bros-outside.jpg";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Button from "./Button";
 
+type Event = {
+  image: StaticImageData;
+  altText: string;
+  name: string;
+  date: string;
+  volunteerLink?: string;
+  paragraphs: string[];
+};
+
 export default function EventsSection() {
-  const events = [
+  const events: Event[] = [
     {
       image: TwoBros,
       altText: "Two Brothers Bar and Grill Outside View",
@@ -37,12 +46,14 @@ export default function EventsSection() {
               <div className="text-5xl absolute inset-0 m-auto text-white text-center flex flex-col items-center gap-4 w-fit h-fit">
                 <h2 className="font-bold">{event.name}</h2>
                 <p className="italic font-semibold">June 22nd 2024</p>
-                <Button
-                  extraStyles="text-2xl"
-                  href={event.volunteerLink}
-                  target="_blank"
-                  text={"Sign Up To Volunteer"}
-                />
+                {event.volunteerLink && (
+                  <Button
+                    extraStyles="text-2xl"
+                    href={event.volunteerLink}
+                    target="_blank"
+                    text={"Sign Up To Volunteer"}
+                  />
+                )}
               </div>
             </div>
             <div className="flex flex-col justify-center bg-black text-white text-right col-span-5 lg:col-span-2 lg:text-xl p-8 italic gap-3">
