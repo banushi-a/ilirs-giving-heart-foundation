e45d2c79fc5bc1387bffe5ccf31a5f115eaea82a
banushi-a <altonbanushi@icloud.com>
2024-06-17 21:28:57 -0400

    Remove Volunteer Sign Up Link

--- src/components/EventsSection.tsx
+++ src/components/EventsSection.tsx
@@ -1,6 +1,4 @@
 import TwoBros from "../public/2bros-outside.jpg";
-import Image from "next/image";
-import Button from "./Button";
 import Event, { type EventProps } from "./Event";
 
 export default function EventsSection() {
@@ -10,7 +8,7 @@ export default function EventsSection() {
       altText: "Two Brothers Bar and Grill Outside View",
       name: "Ilir's Celebration of Life Blockparty Fundraiser",
       date: "June 22nd 2024",
-      volunteerLink: "https://forms.fillout.com/t/nPaMuUDQ9Uus",
+      // volunteerLink: "https://forms.fillout.com/t/nPaMuUDQ9Uus",
       paragraphs: [
         "Once met, never forgotten. Ilir loved brining people together and extending a hand to anyone in need. Let's honor Ilir by coming together for what made him the most happy: family, friends, & music! Join us in front of Two Brother's Bar on Fulton Street for a special tribute Block Party starting at 2PM. An all day celebration of life for the man who wanted everyone to always have a good time.",
         "Appearances by Pink Houses, The Gary Band, Mad Tadders, and special guest Jason Stubbs.",
